d06dfca9942bcfed3d8ff02be8c40d6b24686058
marcus-wong-ics4u1-02-25 <marcus.wong5@student.tdsb.on.ca>
2025-03-03 15:09:15 +0000

    same assignment 1.1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,17 +4,46 @@ public class PracticeProblem {
 
 	}
 
-	public static void q1() {
 		//Write question 1 code here
-	}
+		public static int sum2D(int[][] intArr) {
+			int sum = 0;
+			for (int i = 0; i < intArr.length; i++) {
+				for (int j = 0; j < intArr[i].length; j++) {
+					sum += intArr[i][j];
 
-	public static void q2() {
-		//Write question 2 code here
-	}
+				}
+			}
+			return sum;
 
-	public static void q3() {
-		//Write question 3 code here
-	}
+		}
+		//write question 2 here
+		public static int sumRow(int[][] intArr2, int row) {
+			int sum = 0;
+			for (int col = 0; col < intArr2[row].length; col++) {
+				sum += intArr2[row][col];
+			}
+			return sum;
+
+		}
+		//write question 3 here
+		public static int sumColumn(int[][] intArr3, int col) {
+			int sum = 0;
+			for (int row = 0; row < intArr3.length; row++) {
+				sum += intArr3[row][col];
+			}
+			return sum;
+
+		}
+
+		
+			
+
+	
+	
+
+
+
+	
 
 	public static void q4() {
 		//Write question 4 code here
